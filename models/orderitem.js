44f976dbeae8239fadf979dbef2e0dbe05634c03
Gabriel Gitonga <gabworks51@gmail.com>
2021-06-22 21:27:51 +0300

    Adding orders models

--- models/orderitem.js
+++ models/orderitem.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const OrderItem = new Schema({})
+const OrderItem = new Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
 
 exports.OrderItem = mongoose.model("Order", OrderItem )
